70f53a40c41ce0ab96ec1292b535f2ddd74a7192
Priest <Charleschukwuemeka47@gmail.com>
2024-02-17 14:03:52 +0100

    Feat: User login works

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
+
+//Import Needed Images
+import logo from "../../../public/Images/Big whiz Walletassets logo badge.svg";
+
+//Import Needed Components
+import LoginForm from "@/components/AuthComponents/LoginForm";
 const page = () => {
     return ( 
-        <main>
-            
+        <main className="createBackground h-screen flex items-center justify-center">
+            <div className="w-[90%] sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%] bg-white shadow py-8 px-4 md:px-6 xl:px-8">
+                <div className="flex gap-x-1 items-center">
+                    <Image src={logo} alt="Capital Sphere Bank Logo" priority={true} className="size-8 md:size-10 xl:size-12"/>
+                    <div className="text-[#1C1F33] font-semibold text-xs sm:text-sm xl:text-base">
+                        <p>Wealth</p>
+                        <p className="-mt-1">Assets</p>
+                    </div>
+                </div>
+                <div className="text-[#161618] mt-10">
+                    <p className="text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-bold">Sign In</p>
+                    <p className="text-xs sm:text-sm xl:text-base font-semibold mt-2">Welcome back</p>
+                </div>
+                <LoginForm />
+            </div>
         </main>
      );
 }
---
+++ src/components/AuthComponents/LoginForm.tsx
@@ -0,0 +1,131 @@
+"use client";
+import { FormEvent, useState } from "react";
+import Link from "next/link";
+import { toast } from "sonner";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+//Import Needed Icons
+import { Eye, EyeSlash } from 'iconsax-react';
+
+//Import Needed Components
+import Button from "../molecules/Button";
+
+type InitialStateProps = {
+  email: string;
+  password: string;
+  role: string;
+};
+const initialState: InitialStateProps = {
+  email: "",
+  password: "",
+  role: "user"
+};
+
+const LoginForm = () => {
+  const router = useRouter();
+  //Loading state for the form
+  const [loading, setLoading] = useState<boolean>(false);
+  const [seen, setSeen] = useState<boolean>(false);
+  const handleSeePassword = () => {
+    setSeen((prev) => !prev);
+  };
+  const [tryForgotten, setTryForgotten] = useState<boolean>(false)
+  const [state, setState] = useState(initialState);
+  //Function for the State Changing
+  const handleChange = (event: any) => {
+    setState({ ...state, [event.target.name]: event.target.value });
+  };
+  //Function for the Form Reset
+  const handleFormReset = () => {
+    setState(initialState);
+  };
+  const onSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    setLoading(true);
+
+    try {
+      const callback = await signIn("credentials", {
+        ...state,
+        redirect: false,
+      });
+
+      if (callback?.ok && !callback?.error) {
+        setLoading(false);
+        toast.success("Welcome");
+        handleFormReset();
+        router.push("/user/dashboard");
+        
+      } else if (callback?.error) {
+        setLoading(false);
+        handleFormReset();
+        toast.error("Wrong Email or Password");
+        setTryForgotten(true)
+      }
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+      handleFormReset();
+      toast.error("An error occurred during sign-in");
+      setTryForgotten(true)
+    }
+  };
+
+  return (
+    <main className="mt-12 text-xs md:text-sm xl:text-base text-[#161618]">
+      <form onSubmit={onSubmit}>
+        <div className="flex flex-col gap-y-1">
+          <label className="cursor-pointer" htmlFor="email">
+            Email
+          </label>
+          <input
+            value={state.email}
+            type="email"
+            name="email"
+            id="email"
+            className="border border-[#E6E7E8] px-2 xl:px-4 py-2 md:py-3 focus:border-primary rounded-md focus:outline-none"
+            placeholder="example@gmail.com"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="relative flex flex-col gap-y-1 mt-6">
+          <label className="cursor-pointer" htmlFor="phoneNumber">
+            Password
+          </label>
+          <input
+            type={seen ? "text" : "password"}
+            value={state.password}
+            name="password"
+            id="password"
+            className="border border-[#E6E7E8] px-2 xl:px-4 py-2 md:py-3 focus:border-primary rounded-md focus:outline-none"
+            placeholder="xxxxxxxxxxxxx"
+            onChange={handleChange}
+          />
+          <div
+            className="absolute top-[55%] right-4 cursor-pointer text-base sm:text-lg md:text-xl xl:text-2xl"
+            onClick={handleSeePassword}
+          >
+            {seen ? <EyeSlash size="18" /> : <Eye size="18" />}
+          </div>
+        </div>
+        <p className="mt-4">
+          First time using Wealth Assets?
+          <span className="text-primary hover:underline duration-500">
+            <Link href="/create"> Sign up</Link>
+          </span>
+        </p>
+        {tryForgotten && 
+        <p className="mt-4">
+          <span className="text-primary hover:underline duration-500">
+            <Link href="/forgotPassword"> Forgot Password?</Link>
+          </span>
+        </p>
+        }
+        
+        <Button type="submit" text="Login" loading={loading} />
+      </form>
+    </main>
+  );
+};
+
+export default LoginForm;
