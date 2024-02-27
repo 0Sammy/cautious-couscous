dffef52a5cb58b22ef34410dfa94465bdf680a7d
Priest <Charleschukwuemeka47@gmail.com>
2024-02-27 02:00:01 +0100

    Feat: Done with transaction history

--- src/app/user/coin/page.tsx
+++ src/app/user/coin/page.tsx
@@ -1,5 +1,6 @@
 //Import Needed Components
 import CoinOverview from "@/components/CoinComponents/CoinOverview";
+import TransactionHistory from "@/components/CoinComponents/TransactionHistory";
 
 
 const page = () => {
@@ -7,6 +8,7 @@ const page = () => {
     return ( 
         <main className="p-2 md:p-4 xl:p-6">
             <CoinOverview />
+            <TransactionHistory />
         </main>
      );
 }
