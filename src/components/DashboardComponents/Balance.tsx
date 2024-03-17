f116ca3c7fa56d0dd69c7cc44b5784521a9ae497
Priest <Charleschukwuemeka47@gmail.com>
2024-03-17 16:59:43 +0100

    Fix: Make sure the values are no longer 0, before showing the dashboard

--- src/components/DashboardComponents/Balance.tsx
+++ src/components/DashboardComponents/Balance.tsx
@@ -5,7 +5,7 @@ import { useBalanceStore } from "@/store/balance";
 import { usePriceStore } from "@/store/prices";
 
 //Import Needed Icons
-import { Wallet3 } from "iconsax-react";
+import { Refresh2, Wallet3 } from "iconsax-react";
 
 
 const Balance = () => {
@@ -13,6 +13,8 @@ const Balance = () => {
     const {btcPrice, ethPrice, bnbPrice, trxPrice, usdtPrice, adaPrice, solPrice, ltcPrice, dogePrice } = usePriceStore()
     const {btcBalance, ethBalance, binanceBalance, tronBalance, usdttBalance, usdteBalance, adaBalance, solBalance, liteBalance, dogeBalance} = useBalanceStore()
     const [total, setTotal] = useState<number>(0)
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
     useEffect(() => {
         const btcTotal = btcBalance * btcPrice
         const ethTotal = ethBalance * ethPrice
@@ -27,24 +29,38 @@ const Balance = () => {
         
         const total = (btcTotal + ethTotal + bnbTotal + tronTotal + usdttTotal + usdteTotal + adaTotal + solTotal + liteTotal + dogeTotal)
         setTotal(total)
+        
+        if(total > 0){
+          setIsLoading(false)   
+        } else{
+            window.location.reload()
+        }
 
     },[adaBalance, adaPrice, binanceBalance, bnbPrice, btcBalance, btcPrice, dogeBalance, dogePrice, ethBalance, ethPrice, liteBalance, ltcPrice, solBalance, solPrice, tronBalance, trxPrice, usdtPrice, usdteBalance, usdttBalance])
     return ( 
         <main className="bg-[#160959] rounded-2xl p-4 md:p-6 xl:p-8 flex flex-col gap-y-10 text-white shadow-xl">
-            <div className="flex items-center justify-between">
-                <div className="flex flex-col gap-y-1">
-                   <p className="text-xs md:text-sm xl:text-base">Total Balance</p>
-                   <p className="text-lg md:text-xl xl:text-2xl font-semibold">${(total).toLocaleString()}</p> 
+            {isLoading ? (
+                <div className="fixed top-0 left-0 flex items-center justify-center bg-black bg-opacity-90 z-[9999] h-screen w-full">
+                    <Refresh2 size="40" color="#FF8A65" className="animate-spin" />
                 </div>
-                <Wallet3 size="30" variant="Bold"/>
-            </div>
-            <div className="flex flex-col gap-y-5 font-semibold text-xs md:text-sm xl:text-base">
-                <div className="flex gap-x-3">
-                    <Link href="/user/overview" className="w-1/2 bg-white text-[#160959] py-2 md:py-3 text-center rounded-2xl border border-white hover:bg-inherit hover:text-white duration-500">Receive</Link>
-                    <Link href="/user/overview" className="w-1/2 bg-white text-[#160959] py-2 md:py-3 text-center rounded-2xl border border-white hover:bg-inherit hover:text-white duration-500">Send</Link>
+            ) : (
+            <>
+                <div className="flex items-center justify-between">
+                    <div className="flex flex-col gap-y-1">
+                       <p className="text-xs md:text-sm xl:text-base">Total Balance</p>
+                       <p className="text-lg md:text-xl xl:text-2xl font-semibold">${(total).toLocaleString()}</p> 
+                    </div>
+                    <Wallet3 size="30" variant="Bold"/>
                 </div>
-                <Link href="https://www.Moonpay.com" target="_blank" className="mx-auto w-1/2 bg-white text-[#160959] py-2 md:py-3 text-center rounded-2xl border border-white hover:bg-inherit hover:text-white duration-500">Buy</Link>
-            </div>
+                <div className="flex flex-col gap-y-5 font-semibold text-xs md:text-sm xl:text-base">
+                    <div className="flex gap-x-3">
+                        <Link href="/user/overview" className="w-1/2 bg-white text-[#160959] py-2 md:py-3 text-center rounded-2xl border border-white hover:bg-inherit hover:text-white duration-500">Receive</Link>
+                        <Link href="/user/overview" className="w-1/2 bg-white text-[#160959] py-2 md:py-3 text-center rounded-2xl border border-white hover:bg-inherit hover:text-white duration-500">Send</Link>
+                    </div>
+                    <Link href="https://www.moonpay.com" target="_blank" className="mx-auto w-1/2 bg-white text-[#160959] py-2 md:py-3 text-center rounded-2xl border border-white hover:bg-inherit hover:text-white duration-500">Buy</Link>
+                </div>
+            </>
+            )}
         </main>
      );
 }
