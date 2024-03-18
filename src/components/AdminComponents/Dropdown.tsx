00dfe18fddec533dc6dd72a7afc999699909659a
Priest <Charleschukwuemeka47@gmail.com>
2024-03-18 12:47:59 +0100

    Feat: Add a search option for users

--- src/components/AdminComponents/Dropdown.tsx
+++ src/components/AdminComponents/Dropdown.tsx
@@ -5,6 +5,7 @@ import { useTransactionStore } from '@/store/adminTransactionStore';
 const Dropdown = ({ allUsers }: any) => {
   const { updateUserId, updateUserEmail, updateUserName } = useTransactionStore();
   const [selectedUserId, setSelectedUserId] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
 
   const handleSelectChange = (event: any) => {
     const selectedId = event.target.value;
@@ -20,12 +21,27 @@ const Dropdown = ({ allUsers }: any) => {
     console.log({ selectedUser });
   };
 
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  // Filter users based on search query
+  const filteredUsers = allUsers.filter((user: any) => {
+    return user.id.includes(searchQuery) || user.email.includes(searchQuery);
+  });
+
   return (
     <div className='flex flex-col gap-y-1'>
       <label htmlFor="userDropdown" className='text-sm lg:text-base cursor-pointer'>Select Client</label>
+      <input 
+        type="text" 
+        placeholder="Search by userId or email" 
+        value={searchQuery} 
+        onChange={handleSearchChange} 
+        className="border border-[#E6E7E8] px-2 xl:px-4 py-2 md:py-3 focus:border-primary rounded-md focus:outline-none"
+      />
       <select id="userDropdown" onChange={handleSelectChange} value={selectedUserId} className='capitalize cursor-pointer border border-[#E6E7E8] px-2 xl:px-4 py-2 md:py-3 focus:border-primary rounded-md focus:outline-none'>
-        <option value="">Select a user</option>
-        {allUsers.map((user: any) => (
+        {filteredUsers.map((user: any) => (
           <option key={user.id} value={user.id}>
             {`${user.firstName} ${user.lastName}`}
           </option>
