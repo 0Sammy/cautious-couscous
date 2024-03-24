9ef1488d54f449348aac473663ee37ee0a130c3e
Priest <Charleschukwuemeka47@gmail.com>
2024-03-24 04:29:52 +0100

    Feat: Add Our Vision

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,10 +2,11 @@
 import Nav from "@/components/PagesComponents/Nav";
 import CTA from "@/components/BuyComponents/CTA";
 import OurPurpose from "@/components/AboutUsComponents/OurPurpose";
-
+import OurVision from "@/components/AboutUsComponents/OurVision";
 
 
 import Footer from "@/components/PagesComponents/Footer";
+
 
 
 const page = () => {
@@ -14,6 +15,7 @@ const page = () => {
             <Nav />
             <div className="lg:w-[90%] xl:w-[80%] 2xl:w-[65%] mx-auto mt-32 md:mt-40 lg:mt-48 xl:mt-52 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-16">
                 <OurPurpose />
+                <OurVision />
                 <CTA />
                 <Footer />
             </div>
