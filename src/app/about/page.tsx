2c8af7c19ee22f3bc5d7376f84e5c373599f5ac2
Priest <Charleschukwuemeka47@gmail.com>
2024-03-24 03:58:16 +0100

    Feat: Add the purpose section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,20 @@
 //Import Needed Components
-import Nav from "@/components/PagesComponents/Nav"
-import Footer from "@/components/PagesComponents/Footer"
+import Nav from "@/components/PagesComponents/Nav";
+import CTA from "@/components/BuyComponents/CTA";
+import OurPurpose from "@/components/AboutUsComponents/OurPurpose";
+
+
+
+import Footer from "@/components/PagesComponents/Footer";
+
 
 const page = () => {
     return ( 
         <main className="bg-white">
             <Nav />
             <div className="lg:w-[90%] xl:w-[80%] 2xl:w-[65%] mx-auto mt-32 md:mt-40 lg:mt-48 xl:mt-52 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-16">
+                <OurPurpose />
+                <CTA />
                 <Footer />
             </div>
         </main>
---
+++ src/components/AboutUsComponents/OurPurpose.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image"; 
+
+
+//Import Needed Images
+import flashImg from "../../../public/Images/aboutUs.png";
+
+const OurPurpose = () => {
+    return ( 
+        <main className="rounded-[20px] md:rounded-[30px] py-10 md:py-20 px-4 sm:px-6 md:px-8 xl:px-10 bg-primary text-white flex flex-col gap-y-5 items-center md:items-start md:gap-y-0 md:flex-row md:justify-between md:gap-x-5">
+            <h3 className="font-semibold text-xl md:text-2xl xl:text-3xl w-full md:w-[25%]">Our purpose</h3>
+            <div className="flex flex-col items-center md:flex-row">
+                <div className="flex flex-col gap-y-5">
+                  <p className="text-3xl md:text-4xl lg:text-5xl xl:text-[3.5rem] font-bold">Freedom Foundations</p>  
+                  <p className="text-sm md:text-base xl:text-lg font-medium">We want to give everyone the freedom to truly own, by building the foundations for the future of the free web.</p>
+                </div>
+                <Image src={flashImg} alt="Image" className="size-52" />
+            </div>
+        </main>
+     );
+}
+ 
+export default OurPurpose;
