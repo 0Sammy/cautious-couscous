aff80d9832c4809440bc6bda770f6bb24fb3aa57
Priest <Charleschukwuemeka47@gmail.com>
2024-03-25 11:01:01 +0100

    Fix: Adjusted the css style for the installation prompt

--- src/components/DashboardComponents/InstallationPrompt.tsx
+++ src/components/DashboardComponents/InstallationPrompt.tsx
@@ -71,10 +71,10 @@ const InstallationPrompt = () => {
       <div className="fixed bottom-0 left-0 w-full bg-white py-4 shadow-lg z-[99] flex items-end justify-around px-4 md:px-6 xl:px-8">
         <div className="flex flex-col gap-y-1">
           <p>To install the Wealth Assets App in Your iOS Device, follow these steps:</p>
-          <ul className="pl-4 text-sm">
-            <li className="flex items-center">Tap the <strong className="ml-[2px] text-primary flex items-center">Share<DirectSend size="20" className="mx-[2px]"/></strong> icon at the bottom center of the screen.</li>
+          <ul className="pl-2 flex flex-col gap-y-2">
+            <li className="flex items-center">Tap the <strong className="mx-[2px] text-primary flex items-center">Share<DirectSend size="18" className="mx-[2px]"/></strong> icon at the bottom center of the screen.</li>
             <li>Scroll down and select <strong className="text-primary">Add to Home Screen</strong>.</li>
-            <li className="flex items-center">Then tap <strong className="ml-[2px] text-primary flex items-center">Add<AddSquare size="20" className="mx-[2px]"/></strong>.</li>
+            <li className="flex items-center">Then tap <strong className="mx-[2px] text-primary flex items-center">Add<AddSquare size="18 " className="mx-[2px]"/></strong>.</li>
           </ul>
         </div>
         <CloseSquare size="30" className="text-red-600 cursor-pointer" variant="Bold" onClick={() => setIsAppleDevice(false)} />
