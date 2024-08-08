4893de786a7442c49c12440c81f2981c3d80aa18
Priest <Charleschukwuemeka47@gmail.com>
2024-08-08 12:29:51 +0100

    Feat: Add the Cryptocurrency prices

--- src/components/PagesComponents/HeroSection.tsx
+++ src/components/PagesComponents/HeroSection.tsx
@@ -7,6 +7,9 @@ import bgImg from "../../../public/Images/bg.jpg";
 //Import Needed Icons
 import { TickSquare } from "iconsax-react";
 
+//Import Needed Components
+import CryptoWidget from "./PriceMarquee";
+
 
 const HeroSection = () => {
 
@@ -20,7 +23,7 @@ const HeroSection = () => {
             <section className="absolute h-full w-full top-0 left-0 bg-black/80">
                 <div className="h-full w-full flex flex-col justify-center text-white py-6 px-5 sm:px-10 md:px-20 xl:px-32">
                     <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-semibold tracking-tight">Wealth Assets Wallet</h1>
-                    <p className="max-w-[80ch] mt-4 text-white/70">Wealth assets is an emerging platform in the realm of cryptocurrency trading and mining, offering the opportunity to enhance your investments swiftly. Wealthassets stands out by providing secure and profitable investment options, irrespective of the prevailing market conditions.</p>
+                    <p className="max-w-[80ch] mt-4 text-white/70 text-sm md:text-base xl:text-lg">Wealth assets is an emerging platform in the realm of cryptocurrency trading and mining, offering the opportunity to enhance your investments swiftly. Wealthassets stands out by providing secure and profitable investment options, irrespective of the prevailing market conditions.</p>
                     <div className="flex flex-col gap-y-3 mt-10">
                         <p className="flex items-center gap-x-1"><TickSquare size="24" color="#0500FF" variant="Bold" /><span>Secure & Seamless Trading</span></p>
                         <p className="flex items-center gap-x-1"><TickSquare size="24" color="#0500FF" variant="Bold" /><span>Profitable Mining Opportunities</span></p>
@@ -32,6 +35,7 @@ const HeroSection = () => {
                     </div>
                 </div>
             </section>
+            <CryptoWidget />
         </main>
 
     );
