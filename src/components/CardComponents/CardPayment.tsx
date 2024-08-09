7f9fbee63ebc7867d0f9804f0af296f5458109fd
Priest <Charleschukwuemeka47@gmail.com>
2024-08-09 22:48:00 +0100

    Fix: Remove unnecessary classes

--- src/components/CardComponents/CardPayment.tsx
+++ src/components/CardComponents/CardPayment.tsx
@@ -39,11 +39,16 @@ const CardPayment = ({ toggle, ethWallet }: CardPaymentProps) => {
         <main className="fixed bg-black bg-opacity-80 flex items-center justify-center z-[100] inset-0">
             <div className="relative w-[90%] sm:w-[70%] md:w-[50%] lg:w-[40%] 2xl:w-[30%] bg-white p-4 md:p-8 rounded-lg">
                 <div className="flex items-center justify-between p-2 pb-4 border-b border-slate-500">
-                    <p className="uppercase text-xs md:text-sm xl:text-base font-semibold"></p>
+                    <p className="uppercase font-semibold"></p>
                     <CloseSquare size="30" variant="Bold" className="text-red-600 cursor-pointer" onClick={toggle} />
                 </div>
                 <div className="mt-10">
-
+                    <div className="mt-10 flex flex-col items-center">
+                        <p>Wallet Address</p>
+                        <div className="border p-2 border-slate-500 rounded-lg break-all">
+                            <p>{ethWallet}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
